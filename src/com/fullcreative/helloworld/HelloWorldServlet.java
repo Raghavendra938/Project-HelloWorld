98c634f636437c95416cb9fb596e07e38cd43570
raghavendra938 <raghavendra.kumar@adaptavantcloud.com>
2017-02-21 11:51:46 +0530

    Variable changes

--- src/com/fullcreative/helloworld/HelloWorldServlet.java
+++ src/com/fullcreative/helloworld/HelloWorldServlet.java
@@ -7,9 +7,11 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class HelloWorldServlet extends HttpServlet {
 	String str = null;
+	String str1 = null;
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		str = "Hello, World";
+		str1 = "Hello, Person";
 		resp.getWriter().println(str);
 	}
 }
