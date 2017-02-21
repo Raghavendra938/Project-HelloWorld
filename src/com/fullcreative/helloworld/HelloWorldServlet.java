34a29153c6e0783b31a8d6ec5770696e5ad35957
raghavendra938 <raghavendra.kumar@adaptavantcloud.com>
2017-02-21 11:42:16 +0530

    Variable made as global

--- src/com/fullcreative/helloworld/HelloWorldServlet.java
+++ src/com/fullcreative/helloworld/HelloWorldServlet.java
@@ -1,13 +1,15 @@
 package com.fullcreative.helloworld;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class HelloWorldServlet extends HttpServlet {
+	String str = null;
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		String str = "Hello, World";
+		str = "Hello, World";
 		resp.getWriter().println(str);
 	}
 }
